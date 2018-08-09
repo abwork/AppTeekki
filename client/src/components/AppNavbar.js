19a6d4bb1d0639d8c66b2a3203bae450b1a0e091
Abdi Muleta <abdi.muleta@integrify.io>
2018-08-09 13:48:39 +0300

    clientside dev setup commit

---
+++ client/src/components/AppNavbar.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    Container
+}
+
+export default class AppNavbar extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
