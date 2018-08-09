fa80d7d5676c07d497005d4369efbf4032dbfec9
Abdi Muleta <abdi.muleta@integrify.io>
2018-08-09 11:57:41 +0300

    Backend commit
    Backend of the app using Express and Mongoose done and can interact with MongoDB.

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb://Abdisome:Drugs2018@ds217002.mlab.com:17002/medicinesdb'
+}
---
+++ models/medicine.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//create schema
+const MedicineSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    // generic_name: {
+    //     type: String,
+    //     required: true
+    // },
+    general_information: {
+        type: String,
+        required: true
+    },
+    side_effects: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+}); 
+
+module.exports = Medicine = mongoose.model('medicine', MedicineSchema);
