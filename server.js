5698a75b069658c1ea2da5c1386b9fd5acebc64d
Abdi Muleta <abdi.muleta@integrify.io>
2018-08-19 08:19:40 +0300

    make it delpoy

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ if (process.env.NODE_ENV === 'production') {
     app.get('*', (req, res) => {
       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
-  }
+  };
 
 //port for the server
 const port = process.env.PORT || 5000;
